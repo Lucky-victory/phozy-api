6254b8ba98c64e658de1c8775cbd85157188af33
Lucky-victory <blizwonder25@gmail.com>
2022-07-26 11:03:18 +0100

    update

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,13 +4,14 @@ import { IUsersResult } from "../interfaces/users";
 const { JWT_SECRET_KEY } = process.env;
 
 export const generateJwtToken = (user: IUsersResult): string => {
-  
-  const token = jwtSign(JSON.stringify(user), (JWT_SECRET_KEY as string));
+  const token = jwtSign(JSON.stringify(user), JWT_SECRET_KEY as string);
 
   return token;
 };
-export const removePropFromObject = (obj:{[key:string]:any},prop:string) => {
-  
+export const removePropFromObject = (
+  obj: { [key: string]: unknown },
+  prop: string
+): IUsersResult => {
   obj[prop] ? delete obj[prop] : null;
-  return obj;
-}
+  return obj as unknown as IUsersResult;
+};
